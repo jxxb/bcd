bf3e035ffda2d3e9b03f8f4bb012f8a71be590f6
DortchCasey <caseyd199@gmail.com>
2021-01-21 10:04:26 -0700

    team activity 2

---
+++ src/js/productdata.js
@@ -0,0 +1,20 @@
+  function convertToJson(t) {
+    if (t.ok) return t.json();
+    throw new Error("Bad Response");
+  }
+
+  export default class ProductData {
+    constructor(category){
+        this.category = category;
+        this.path = `../json/${this.category}.json`;
+     }
+    
+      getData (){
+          return fetch(this.path).then(convertToJson).then((data)=>data);
+      }
+    async findProductById(id){
+        const products = await this.getData();
+        return products.find((item)=>item.Id === id);
+    }
+
+    }
